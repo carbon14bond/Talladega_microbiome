c21c82471923a537daac0ef999f59d902eae8519
Charles Bond <72367119+carbon14bond@users.noreply.github.com>
2025-05-20 12:41:29 -0500

    Update Export_16S_for_picrust2.R
    trimming redundant

--- Analysis/picrust2_prep/Export_16S_for_picrust2.R
+++ Analysis/picrust2_prep/Export_16S_for_picrust2.R
@@ -20,14 +20,3 @@ tasvtab<-as.data.frame(t(asvtab))
 tasvtab <- tibble::rownames_to_column(tasvtab, "ASV_ID")
 library(readr)
 write_tsv(tasvtab,"TalSyn_16S_dada2_ASV_counts_for_pricust2_05.19.2025.tsv")
-
-### now filter fastas to remove ASVs not included above:
-library("Biostrings")
-s <- readDNAStringSet("TalSyn_16S_dada2_ASVs_04.25.2025.fasta")
-
-ns<- s[names(s) %in% asvlist]
-head(ns)
-library(ShortRead)
-fasta_dir <- file.path(getwd(), "refs")
-outfile <- file.path(dirname(fasta_dir), "TalSyn_16S_ASV_fasta_for_picrust2.fasta")
-writeFasta(ns, outfile, mode = "a")
